15d2a82a4da4260eca120f3344d778b2699a4157
Jesus Samano <doomlord97@gmail.com>
2020-11-27 11:58:19 -0600

    Aplicación final1

--- src/main/java/com/gimnasio/app/AdministradorGymApplication.java
+++ src/main/java/com/gimnasio/app/AdministradorGymApplication.java
@@ -41,7 +41,7 @@ public class AdministradorGymApplication implements CommandLineRunner {
 				}
 			}
 			emailBody.setContent(texto.toString());
-			emailBody.setEmail("yoyo_jueanes03@gmail.com");
+			emailBody.setEmail("yoyo_juanes03@gmail.com");
 			emailBody.setSubject("Lista de clientes con suscripcion vencida");
 			emailPort.sendEmail(emailBody);
 
